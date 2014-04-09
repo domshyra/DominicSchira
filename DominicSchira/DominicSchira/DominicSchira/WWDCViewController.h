95b320f145559febd5fbc5001eaf3c5906b9c6df
dom <domshyra@gmail.com>
2014-04-08 20:37:02 -0700

    Initial Commit

---
+++ DominicSchira/DominicSchira/DominicSchira/WWDCViewController.h
@@ -0,0 +1,13 @@
+//
+//  WWDCViewController.h
+//  DominicSchira
+//
+//  Created by Dominic Schira on 4/8/14.
+//  Copyright (c) 2014 Dominic Schira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WWDCViewController : UIViewController
+
+@end
---
+++ DominicSchira/DominicSchira/DominicSchiraTests/DominicSchiraTests.m
@@ -0,0 +1,34 @@
+//
+//  DominicSchiraTests.m
+//  DominicSchiraTests
+//
+//  Created by Dominic Schira on 4/8/14.
+//  Copyright (c) 2014 Dominic Schira. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface DominicSchiraTests : XCTestCase
+
+@end
+
+@implementation DominicSchiraTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
