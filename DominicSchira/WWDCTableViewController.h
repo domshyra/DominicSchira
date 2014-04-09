9dadd051773c401a7e33a47f0e15ade37bbcd0d0
dom <domshyra@gmail.com>
2014-04-08 20:36:14 -0700

    Added table view controller

---
+++ DominicSchira/WWDCTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  WWDCTableViewController.h
+//  DominicSchira
+//
+//  Created by Dominic Schira on 4/8/14.
+//  Copyright (c) 2014 Dominic Schira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WWDCTableViewController : UITableViewController
+
+@end
